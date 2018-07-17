c77e9285017fa46a4845c783e5332f861f59280d
Nelly Sattari <nsattari@challenger.com.au>
2018-07-17 14:41:13 +1000

    fixed script

--- src/Feature/PromotionComponent/code/Scripts/handleBars.compile.js
+++ src/Feature/PromotionComponent/code/Scripts/handleBars.compile.js
@@ -8,28 +8,28 @@ $(document).ready(() => {
 
     // Promo
     const json = {
-        "themeName": "promotion-light-theme",///alternative:promotion-dark-theme or promotion-light-theme
-        "alignment": "end",//alternative:end
-        "head-class": "promo-heading",//alternative:
-        "copy-text-class": "promo-copy-text",
-        "target": "_blank",//alternative:"",
-        "backgroundImageAlt":"whatever",
-        "imageHeight":"536px"
+        'themeName': 'promotion-light-theme',///alternative:promotion-dark-theme or promotion-light-theme
+        'alignment': 'end',//alternative:end
+        'head-class': 'promo-heading',//alternative:
+        'copy-text-class': 'promo-copy-text',
+        'target': '_blank',//alternative:"",
+        'backgroundImageAlt':'whatever',
+        'imageHeight':'536px'
 
     };
-    if (json.themeName.trim()=="" ) {json.themeName="default";}
+    if (json.themeName.trim()=='' ) {json.themeName='default';}
     var cta = 1; //alternative: 0
     if (!cta) {
-        json["head-class"] = "promo-heading-no-cta";
-        json["copy-text-class"] = "lead";
-        json["display"]="d-none";
+        json['head-class'] = 'promo-heading-no-cta';
+        json['copy-text-class'] = 'lead';
+        json['display']='d-none';
     }
     // if hasmobilebackgroundimage d-none
-    $(".component.full-width-promotion").wrap("<script id='handlebars-template' type='text/x-handlebars-template'></script>");
-    var handlebarsTemplate = $("#handlebars-template").html();
+    $('.component.full-width-promotion').wrap('<script id=\'handlebars-template\' type=\'text/x-handlebars-template\'></script>');
+    var handlebarsTemplate = $('#handlebars-template').html();
     var template = Handlebars.compile(handlebarsTemplate);
     var data = template(json);
-    $("#template").html(data);
+    $('#template').html(data);
     $('#handlebars-template').remove();
 
 });
